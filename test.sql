6628efcc31f3c6c466b02c0a75aa314be0535ac9
SemaKaraca <145717584+semakaracablog@users.noreply.github.com>
2024-01-18 22:25:51 +0100

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,4 +1,5 @@
-COPY persons(name, population)
-FROM 'C:\sampledb\city.csv'
-DELIMITER ','
-CSV HEADER;
+SELECT * FROM books
+WHERE quantity > 5000
+ORDER BY unit_price_cents ASC
+LIMIT 3 OFFSET 2
+
