9f6e6b04fa6c2b62ae142486fab85697180d7c10
SemaKaraca <145717584+semakaracablog@users.noreply.github.com>
2024-05-21 22:44:46 +0200

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,7 +1,8 @@
 SELECT * FROM books
 WHERE quantity > 5000
-ORDER BY unit_price_cents ASC
-LIMIT 3 OFFSET 2
+ORDER BY unit_price_cents DESC
+LIMIT 3 OFFSET 2;
 
-SELECT created_at,updated_at
-FROM books
+SELECT * FROM books
+WHERE quantity BETWEEN '900' AND '15000'
+ORDER BY name ASC;
